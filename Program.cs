3816e3220167af5e0a6718c4d006b3334f1c1ce9
Qui Ly <lquiitfuture@gmail.com>
2019-12-15 22:34:36 +0700

    Change Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,34 @@
 
 namespace OverrideMethod
 {
-    class Class1
+    //example 1
+    //override a parent class method under child class
+    /* class Class1
+     {
+         public virtual void show()
+         {
+             Console.WriteLine("Super class show method");
+         }
+     }
+     class Class2 : Class1
+     {
+         public override void show()
+         {
+             Console.WriteLine("Sub class override show method");
+         }
+     }
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             Class2 obj = new Class2();
+             obj.show();
+             Console.ReadKey();
+         }
+     }*/
+    //example 2
+    //execute the superclass method if it is overridden in sub-class
+     class Class1
     {
         public virtual void show()
         {
@@ -13,6 +40,9 @@ namespace OverrideMethod
     {
         public override void show()
         {
+            Class1 cl1 = new Class1();
+            cl1.show();
+            //base.show();
             Console.WriteLine("Sub class override show method");
         }
     }
@@ -25,5 +55,6 @@ namespace OverrideMethod
             Console.ReadKey();
         }
     }
+
 
 }
