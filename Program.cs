3df66df173d74d0575734a3accebaf240ec7e201
Qui Ly <lquiitfuture@gmail.com>
2019-12-15 22:29:28 +0700

    add Project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OverrideMethod
+{
+    class Class1
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Super class show method");
+        }
+    }
+    class Class2 : Class1
+    {
+        public override void show()
+        {
+            Console.WriteLine("Sub class override show method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 obj = new Class2();
+            obj.show();
+            Console.ReadKey();
+        }
+    }
+
+}
